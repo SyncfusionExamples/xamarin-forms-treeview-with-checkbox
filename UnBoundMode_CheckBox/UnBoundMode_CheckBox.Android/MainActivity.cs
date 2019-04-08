25de6339604ba4b038caf55288c08ec11f3dfc4d
Nijamudeenm M <nijamudeensulaiman@syncfusion.com>
2019-04-08 17:08:16 +0530

    Added sample for checkbox unbound_mode

---
+++ UnBoundMode_CheckBox/UnBoundMode_CheckBox.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace UnBoundMode_CheckBox.Droid
+{
+    [Activity(Label = "UnBoundMode_CheckBox", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
